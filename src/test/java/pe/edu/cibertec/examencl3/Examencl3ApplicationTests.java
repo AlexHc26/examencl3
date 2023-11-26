9b7ca90e9c7bb0e64116b9f3c46bbc10edd78386
JuniorM2702 <Junior_aybar27@hotmail.com>
2023-11-26 08:46:44 -0500

    primer commmit

---
+++ src/test/java/pe/edu/cibertec/examencl3/Examencl3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.examencl3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examencl3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
